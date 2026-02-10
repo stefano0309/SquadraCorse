3680201b35bf63fb433ce77b57e406f5d4028753
Stefano Dutto <117385811+stefano0309@users.noreply.github.com>
2026-02-10 15:48:37 +0100

    test4

--- test.py
+++ test.py
@@ -4,34 +4,33 @@ import busio
 import digitalio
 from circuitpython_nrf24l01.rf24 import RF24
 
-# 1. Setup SPI con nomi dei pin più compatibili
-# Usiamo SCLK al posto di SCK per evitare l'AttributeError
+# Configurazione SPI e Pin (Usa SCLK per evitare errori di attributo)
 spi = busio.SPI(board.SCLK, MOSI=board.MOSI, MISO=board.MISO)
-
-# 2. Setup Pin CE e CSN (come nel tuo script originale [cite: 1])
 ce_pin = digitalio.DigitalInOut(board.D22)
 csn_pin = digitalio.DigitalInOut(board.D8)
 
-# 3. Inizializzazione Radio
+# Inizializzazione Radio
 radio = RF24(spi, csn_pin, ce_pin)
 
-# 4. Configurazione speculare all'ESP32 
-radio.channel = 40              # radio.setChannel(40)
-radio.pa_level = 0               # radio.setPALevel(RF24_PA_MAX)
-radio.data_rate = 250            # radio.setDataRate(RF24_250KBPS)
-radio.open_tx_pipe(b"00001")     # radio.openWritingPipe("00001") [cite: 1]
-radio.listen = False             # radio.stopListening() [cite: 3]
+# [cite_start]Configurazione identica al tuo C++ [cite: 1, 2, 3]
+radio.channel = 40                 # radio.setChannel(40)
+radio.pa_level = 0                 # radio.setPALevel(RF24_PA_MAX)
+radio.data_rate = 250              # radio.setDataRate(RF24_250KBPS)
+radio.open_tx_pipe(b"00001")       # const byte address[6] = "00001"
+radio.listen = False               # radio.stopListening()
 
-print("TX pronto su Raspberry Pi")
+print("TX pronto: invio dati all'ESP32...")
 
-# 5. Loop di invio [cite: 4]
-msg = b"1234567890123456789012345678901" # Messaggio di 31 byte [cite: 4]
+# [cite_start]Messaggio di 31 caratteri [cite: 4]
+msg = b"1234567890123456789012345678901"
 
-while True:
-    result = radio.send(msg)
-    if result:
-        print("OK")
-    else:
-        print("FAIL")
-    
-    time.sleep(0.1) # delay(100) [cite: 4]
+try:
+    while True:
+        result = radio.send(msg)
+        if result:
+            print("Inviato: OK")
+        else:
+            print("Inviato: FAIL (nessun ACK dall'ESP32)")
+        time.sleep(0.1) # delay(100)
+except KeyboardInterrupt:
+    print("\nChiusura...")
